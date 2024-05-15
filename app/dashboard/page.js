ec5457803f3a2d3da7d61e67a5dac85c5d8bf601
21MoududAhammed <21moudud@gmail.com>
2024-05-15 14:32:54 +0600

    dashboard and settings routes are added

---
+++ app/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function DashBoard() {
+  return (
+    <div>
+      <h1>This is a DashBoard Page.</h1>
+    </div>
+  );
+}
---
+++ app/dashboard/settings/page.js
@@ -0,0 +1,7 @@
+export default function Settings() {
+  return (
+    <div>
+      <h1>This is a settings page.</h1>
+    </div>
+  );
+}
